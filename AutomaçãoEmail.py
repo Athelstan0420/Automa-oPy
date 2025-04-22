7248f659e62e82ecc105b59bae2d29b3a02ce0fc
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-04-22 03:12:22 -0300

    Atualizar o AutomaçãoEmail.py

--- AutomaçãoEmail.py
+++ AutomaçãoEmail.py
@@ -51,3 +51,49 @@ def enviar_email():
         print("ERRO!")
 enviar_email()
 
+
+
+
+
+
+"""
+#Estrutura utilizada para envio de emails:
+
+#-------------------------------------------
+
+#bibliotecas:
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+
+def enviar_email():
+
+    #Variáveis:
+    remetente = 'seuemail@gmail.com'
+    senha = 'suasenha'
+    destinatario = 'destinatario@email.com'
+    assunto = 'Bom dia!'
+    corpo = 'Este é um e-mail automático enviado às 6h da manhã.'
+
+    msg = MIMEMultipart()
+    msg['From'] = remetente
+    msg['To'] = destinatario
+    msg['Subject'] = assunto
+    msg.attach(MIMEText(corpo, 'plain'))
+
+    try:
+        servidor = smtplib.SMTP('smtp.gmail.com', 587)
+        servidor.starttls()
+        servidor.login(remetente, senha)
+        texto = msg.as_string()
+        servidor.sendmail(remetente, destinatario, texto)
+        servidor.quit()
+        print("E-mail enviado com sucesso!")
+    except Exception as e:
+        print("Erro ao enviar e-mail:", e)
+enviar_email()
+
+"""
