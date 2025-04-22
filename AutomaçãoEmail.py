031922d66096cff09055a37d772df8bda69b8b64
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-04-22 03:15:15 -0300

    Atualizar o AutomaçãoEmail.py

--- AutomaçãoEmail.py
+++ AutomaçãoEmail.py
@@ -8,24 +8,17 @@ import openpyxl
 def enviar_email():
     try:
         #Variáveis para armazenar o remetente, senha, destinatário, assunto, corpo
-        workbook = openpyxl.load_workbook('/home/adriel/Documentos/Develop/AutomacaoEmail/testeautomacao.xlsx') #Para carregar a planilha;
+        workbook = openpyxl.load_workbook('/.xlsv') #Para carregar a planilha;
         pagina_clientes = workbook['Página1'] #Página da planilha;
         for linha in pagina_clientes.iter_rows(min_row=2):#Linha minima que deve começar a ler os dados;
-            remetente = "adrielmedeirosaraujo@gmail.com"
-            senha = "gmwk avhw dihk ylxs"
+            remetente = ""
+            senha = ""
             emails = linha[0].value # Extrai o dado da linha/indice;
             to = emails
             destinatario = to
             assunto = "Email automático"
             corpo = """
-                    Olá [Nome],
-
-                    Espero que esteja tudo bem com você!
-
-                    Gostaria de retomar nosso último ponto de contato e verificar se houve algum avanço em relação às pendências mencionadas. Estamos nos aproximando do prazo estipulado, e seria interessante alinharmos os próximos passos para garantir que tudo siga conforme o planejado.
-
-                    Fico à disposição para agendarmos uma breve reunião, caso considere necessário. Me avise qual seria o melhor horário para você nos próximos dias.
-
+                    
                     Agradeço desde já pela atenção e colaboração!
 
                     Atenciosamente,
@@ -57,9 +50,10 @@ enviar_email()
 
 
 """
-#Estrutura utilizada para envio de emails:
-
 #-------------------------------------------
+#Estrutura utilizada para envio de emails:
+#-------------------------------------------
+
 
 #bibliotecas:
 
@@ -72,6 +66,7 @@ from email.mime.multipart import MIMEMultipart
 def enviar_email():
 
     #Variáveis:
+
     remetente = 'seuemail@gmail.com'
     senha = 'suasenha'
     destinatario = 'destinatario@email.com'
